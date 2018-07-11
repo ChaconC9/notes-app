2dd39bd975318d913a381a1efbea751b392706f1
Chacon <a01229574@itesm.mx>
2018-07-10 21:47:36 -0500

    made a working note app
    with help tho

--- js/main.js
+++ js/main.js
@@ -1,85 +1,68 @@
 // Notes list that is going to be shown in the browser
 const notes = [
     {
-      _id: NotesApp.createNoteId(),
-      title: 'Note title goes here!',
-      text: 'This is my very first note text! 😀',
+        _id: NotesApp.createNoteId(),
+        title: 'Note title goes here!',
+        text: 'This is my very first note text! 😀',
     },
     {
-      _id: NotesApp.createNoteId(),
-      title: 'Second awesome note!',
-      text: 'This is my second note text!',
+        _id: NotesApp.createNoteId(),
+        title: 'Second awesome note!',
+        text: 'This is my second note text!',
     }
 ];
 
 const addNewNote = NotesApp.addNewNote;
 
 // Named functions
-<<<<<<< HEAD
-function getNoteById(_id) {
-    // How can we find a note inside our notes array?
-    let found = {};
-    notes.forEach(function(note) {
-        if (note._id === _id) {
-            found = note
-            return note;
-        }
-    });
-    return note;
-    // This should return a single note object if found;
-};
-
-const updateNote = function(_id, title, text, index) {
-    // Replaces object in array with received one.
-    notes[index]._id = _id;
-    notes[index].title = title;
-    notes[index].text = text;
-    notes[index].index = index;
-=======
 function getNoteById(searchId) {
-    let found = {};
-    notes.forEach(function(note) {
+    let foundIndex;
+    notes.forEach(function(note, index) {
         if (note._id === searchId) {
-            found = note;
+            foundIndex = index;
         }
     });
-    return found;
+    return foundIndex;
 };
 
 const updateNote = function(_id, title, text, index) {
-  notes[index]._id = _id;
-  notes[index].text = text;
-  notes[index].title = title;
->>>>>>> upstream/master
+    notes[index]._id = _id;
+    notes[index].text = text;
+    notes[index].title = title;
 };
 
 // Declared functions
 const insertNote = function(_id, title, text) {
-  // This should receive the title, text and _id of a note as params
-  // and as its name says, you need to create a new note object and add it to list
+    let note =
+    {
+        _id: _id,
+        title: title,
+        text: text,
+    };
+    notes.push(note);
 };
 
-const saveNote = () => NotesApp.saveNote(function(_id, title, text) {
-  // Iterate over notes
-  // Validate if note already exists
-  // If note was found, replace values
-  // If not add it
-  // What's do you think missing?
+const saveNote = () => NotesApp.saveNote(function(_id, title, text) { let index = getNoteById(_id);
+    if (index !== undefined) {
+        updateNote(_id, title, text, index)
+    } else {
+        insertNote(_id, title, text);
+    }
 });
 
 const onClickNote = (event) => NotesApp.onClick(event, function(_id) {
-  console.log('Click button Add Note 🖱');
-  let note;
-  // const note;
-  // You will receive an _id of the note
-  // Construct a note object with the same structure of the list
-  // showNote function will render note in editor
-  NotesApp.showNote(note);
+    console.log('Click button Add Note 🖱');
+    let note;
+    let index = getNoteById(_id);
+    if (index !== undefined) {
+        note = notes[index]
+    }
+    NotesApp.showNote(note);
 });
 
 document.addEventListener('DOMContentLoaded', function(event) {
-  console.log('DOM fully loaded and parsed');
-  NotesApp.renderNotesList();
+    console.log('DOM fully loaded and parsed');
+    NotesApp.renderNotesList();
 });
 
 
